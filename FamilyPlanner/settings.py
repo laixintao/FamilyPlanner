6993f4c4a7b88fc04e16972854f403411887e756
赖信涛 <laixintao1995@163.com>
2016-06-21 13:02:08 +0200

    update sqlite3.db

--- FamilyPlanner/settings.py
+++ FamilyPlanner/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'FamilyPlanner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'familyplanner',  # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'mysqlforifenglv0003',
-        'HOST': '127.0.0.1',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '3306',  # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'familyplanner.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
