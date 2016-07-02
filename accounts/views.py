413c8ad48eba7dcf9b381bb55a0e105980e29567
赖信涛 <laixintao1995@163.com>
2016-07-02 13:27:40 +0200

    error message

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render,HttpResponse,HttpResponseRedirect
 from models import User,Family
 from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from django.contrib.admin.views.decorators import staff_member_required
 
 def login(request):
+    haserrror = False
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -13,30 +16,44 @@ def login(request):
                 auth_login(request,user)
                 return HttpResponseRedirect("/user")
             else:
-                return HttpResponse("User is not active.")
+                return render(request,'user-login.html',
+                              {'haserror':True,
+                               'message':"User is not active."})
         else:
-            return HttpResponse("Wrong username or password.")
+            return render(request,'user-login.html',
+                              {'haserror':True,
+                               'message':"Wrong username or password."})
     else:
-        return render(request, 'user-login.html')
+        return render(request, 'user-login.html',{
+            'haserror':False
+        })
 
 def register(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        family = Family.objects.create(family_name = request.POST['familyname'])
-        age = request.POST['age']
-        gender = True
-        if request.POST['gender'] == "true":
-            pass
-        else:
-            gender = False
-        user = User.objects.create_user(username=username,password=password,
-                                        gender=gender,age=age,family_name=family,
-                                        is_staff=True,role=True)
-        user.save()
-        return HttpResponse("You registered successful!")
+        haserror = False
+        try:
+            username = request.POST['username']
+            password = request.POST['password']
+            family = Family.objects.create(family_name = request.POST['familyname'])
+            age = request.POST['age']
+            gender = True
+            if request.POST['gender'] == "true":
+                pass
+            else:
+                gender = False
+            user = User.objects.create_user(username=username,password=password,
+                                            gender=gender,age=age,family_name=family,
+                                            is_staff=True,role=True)
+            user.save()
+            return HttpResponse("You registered successful!")
+        except:
+            haserror = True
+            return render(request,'user-register.html',
+                          {'message':'Sorry, the username is already using or you missed something,try it again!',
+                           'haserror':haserror})
     else:
-        return render(request,'user-register.html')
+        return render(request,'user-register.html',
+                      {'haserror':False})
 
 def index(request):
     user = request.user
